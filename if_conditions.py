74aca4f637f4a9377d65a889125f43ec53363613
MarkCodering <mark.chen.sstm@gmail.com>
2023-07-14 10:01:21 +0800

    files added

---
+++ if_conditions.py
@@ -0,0 +1,12 @@
+condition = True
+
+if condition == True:
+    print("Condition is true")
+else:
+    print("Condition is false")
+    
+# How to improve it?
+if condition:
+    print("Condition is true")
+
+print("Condition is false")
---
+++ variable.py
@@ -0,0 +1,9 @@
+string_var = "Hello World"
+bool_var = True
+float_var = 1.0
+int_var = 1
+
+print(string_var)
+print(bool_var)
+print(float_var)
+print(int_var)
